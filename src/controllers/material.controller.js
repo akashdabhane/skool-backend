3870bb3c28c2e243990240f5f27a23346a4c84d1
Akash Dabhane <akashdabhane10@gmail.com>
2024-09-06 16:59:24 +0530

    material controller and model created

---
+++ src/controllers/material.controller.js
@@ -0,0 +1,120 @@
+import { Material } from '../models/material.model.js';
+import { User } from '../models/user.model.js';
+import { Class } from '../models/class.model.js';
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const createMaterial = asyncHandler(async (req, res) => {
+    const { title, description, classroom } = req.body;
+
+    if ([title, description, classroom].some(field => {
+        return field?.trim() === "" || field?.trim() === undefined
+    })) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const user = await User.findById(req.user._id);
+    if (!user) throw new ApiError(401, "User not found");
+
+    const classroomExists = await Class.findById(classroom);
+    if (!classroomExists) throw new ApiError(404, "Classroom not found");
+
+    const material = await Material.create({
+        title,
+        description,
+        class: classroom,
+        createdBy: user._id
+    });
+
+    if (!material) {
+        throw new ApiError(500, "Failed to create material");
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(201, material, "Material created successfully")
+        );
+})
+
+const deleteMaterial = asyncHandler(async (req, res) => {
+    const { id } = req.params;  // material id
+    const material = await Material.findById(id);
+    if (!material) throw new ApiError(404, "Material not found");
+
+    if (!material.createdBy.equals(req.user._id)) {
+        throw new ApiError(403, "You do not have permission to delete this material");
+    }
+
+    await Material.findByIdAndDelete(id);
+    
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, null, "Material deleted successfully")
+        );
+})
+
+// all materials of classroom
+const getMaterials = asyncHandler(async (req, res) => {
+    const { classroom } = req.params;
+    const materials = await Material.find({ class: classroom });
+    if (!materials) {
+        throw new ApiError(404, "No materials found for this classroom");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, materials, "Materials fetched successfully")
+        );
+})
+
+// get single material
+const getMaterial = asyncHandler(async (req, res) => {
+    const { id } = req.params;  // material id
+    const material = await Material.findById(id);
+    if (!material) throw new ApiError(404, "Material not found");
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, material, "Material fetched successfully")
+        );
+})
+
+const updateMaterial = asyncHandler(async (req, res) => {
+    const { id } = req.params;  // material id
+    const material = await Material.findById(id);
+    if (!material) throw new ApiError(404, "Material not found");
+
+    if (!material.createdBy.equals(req.user._id)) {
+        throw new ApiError(403, "You do not have permission to update this material");
+    }
+
+    const updatedMaterialInfo = await Material.findByIdAndUpdate(id,
+        {
+            $set: req.body
+        },
+        { new: true, runValidators: true }
+    );
+    if (!updatedMaterialInfo) {
+        throw new ApiError(500, "Failed to update material information");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { material: updatedMaterialInfo }, "Material information updated successfully")
+        );
+})
+
+
+export {
+    createMaterial,
+    deleteMaterial,
+    getMaterial,
+    getMaterials,
+    updateMaterial
+}
--- src/models/material.model.js
+++ src/models/material.model.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const materialSchema = new mongoose.Schema({
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
     class: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Class",
@@ -18,30 +23,43 @@ const materialSchema = new mongoose.Schema({
         minlength: 3,
         maxlength: 500
     },
-    materialType: {
-        type: String,
-        required: true,
-        enum: ["Book", "Document", "Video", "Link", "Other"]
-    }, 
-    fileLink: {
+    videoFile: {
         type: [String],
-        required: true,
+        validate: {
+            validator: (v) => {
+                return /\.(mp4|mov|avi|wmv|flv|mkv)$/i.test(v);
+            },
+            message: "Please upload a valid video file. (mp4|mov|avi|wmv|flv|mkv)"
+        }
+    },
+    youtubeVideo: {
+        type: [String],
+    },
+    documentFile: {
+        type: [String],
+        validate: {
+            validator: (v) => {
+                return /\.(pdf|docx|txt|xlsx|pptx)$/i.test(v);
+            },
+            message: "Please upload a valid document file. (pdf|docx|txt|xlsx|pptx)"
+        }
+    },
+    Link: {
+        type: [String],
         validate: {
             validator: (v) => {
                 return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)*\/?$/.test(v);
             },
             message: "Please enter a valid URL."
         }
-    }, 
-    externalLink: {     // for external website etc.
-        type: String,
-        validate: {
-            validator: (v) => {
-                return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)*\/?$/.test(v);
-            },
-            message: "Please enter a valid URL."
-        }
-    }
+    },
 }, { timestamps: true });
+
+materialSchema.pre("save", async function (next) {
+    if (!this.videoFile && !this.documentFile && !this.link && !this.youtubeVideo) {
+        throw new Error("At least one file (video, document, or link) must be provided.");
+    }
+    next();
+})
 
 export const Material = mongoose.model("Material", materialSchema);
